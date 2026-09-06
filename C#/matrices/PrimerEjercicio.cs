e2cc7c4061a1acd0988b670d79a1e0b6c79c2ffe
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-09-06 15:10:42 -0500

    Primer ejercicio matrices C#

---
+++ C#/matrices/PrimerEjercicio.cs
@@ -0,0 +1,24 @@
+using System;
+class PrimerEjercicio
+{
+    static void Main()
+    {
+        int[,] matriz=
+        {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        Console.WriteLine("Matriz 3x3");
+             for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+    
